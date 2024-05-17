6e51048297b2ccc6495e6123c843986fd0e8b471
navneet9971 <Navneetjha2012@gmail.com>
2024-05-17 18:32:48 +0530

    Create skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
 
     <div className="container">
       <Hero />
+      <Skills />
     </div>
     </>
   )
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../../styles/Skills/Skills.css";
+
+const Skills = () => {
+
+    return(
+        <section className="skills-container">
+            <h5>Technical Proficiency</h5>
+
+            <div className='skills-content'>
+                
+            </div>
+        </section>
+    )
+}
+
+export default Skills;
