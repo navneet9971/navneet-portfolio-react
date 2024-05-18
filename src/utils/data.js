7aa6d6ec2f90e70302ad16ae875ec7c8d818a860
navneet9971 <Navneetjha2012@gmail.com>
2024-05-18 14:47:50 +0530

    Done

---
+++ src/utils/data.js
@@ -0,0 +1,45 @@
+export const SKILLS = [
+    {
+        title: 'Frontend',
+        icon: "./assets/images/cv.png",
+        skills: [
+            {skill: "HTML5", percentage: "80%"},
+            {skill: "CSS3", percentage: "90%"},
+            {skill: "JavaScript", percentage: "70%"},
+            {skill: "React.js", percentage: "85%"},
+        ],
+    },
+
+    {
+        title: 'Backend',
+        icon: "../../assets/images/investment.png",
+        skills: [
+            {skill: "Node.js", percentage: "80%"},
+            {skill: "Express.js", percentage: "70%"},
+        ],
+    },
+
+    {
+        title: 'Tools',
+        icon: "../../assets/images/portfolio.png",
+        skills: [
+            {skill: "Git", percentage: "80%"},
+            {skill: "Visual Studio Code", percentage: "70%"},
+            {skill: "Postman", percentage: "70%"},
+            {skill: "Responsive Design", percentage: "70%"},
+            {skill: "Webpack", percentage: "55%"},
+        ],
+    },
+
+    {
+        title: 'Soft Skills',
+        icon: "../../assets/images/statistic.png",
+        skills: [
+            {skill: "Communication", percentage: "80%"},
+            {skill: "Teamwork", percentage: "80%"},
+            {skill: "Leadership", percentage: "85%"},
+            {skill: "Problem Solving", percentage: "75%"},
+            {skill: "Time Management", percentage: "85%"},
+        ],
+    }
+]
