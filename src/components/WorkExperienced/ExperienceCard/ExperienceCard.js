2a615c53c33ade304392b0936f70445a96d647a1
navneet9971 <Navneetjha2012@gmail.com>
2024-05-20 13:31:26 +0530

    Done

--- src/components/WorkExperienced/ExperienceCard/ExperienceCard.js
+++ src/components/WorkExperienced/ExperienceCard/ExperienceCard.js
@@ -10,7 +10,7 @@ const ExperienceCard = ({ details }) => {
             <div className="work-duration">{details.date}</div>
 
             <ul>
-                {details.responsibiliyies.map((item) => (
+                {details.responsibilities.map((item) => (
                     <li key={item}>{item}</li>
                 ))}
             </ul>
